7573937031c9bf0202ba0008a907f432a953dde2
Junaid Khan <jk1240651@gmail.com>
2025-03-13 00:39:46 +0530

    getTotalScoresByUserId api has been updated.

--- controllers/score.js
+++ controllers/score.js
@@ -2,18 +2,33 @@ import dotenv from "dotenv";
 dotenv.config();
 import _ from "lodash";
 import userQuery from "../utils/helper/dbHelper.js";
-
 const getTotalScoresByUserId = async (req, res) => {
     try {
         const { userId } = req.params;
-        
+
+        // Fetch user info first to ensure user exists
+        const getUserInfoQuery = `SELECT id, username, first_name, last_name, email, mobile_number, profile_picture FROM users WHERE id = ?`;
+        const userInfo = await userQuery(getUserInfoQuery, [userId]);
+
+        if (userInfo.length === 0) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        // Fetch total scores from leaderboard
         const getTotalScoresByUserIdQuery = `SELECT * FROM leaderboard WHERE userId = ?`;
         const totalScores = await userQuery(getTotalScoresByUserIdQuery, [userId]);
-        
+
+        if (totalScores.length === 0) {
+            return res.status(404).json({ message: "Score not found" });
+        }
+
+        // Attach user info to scores
+        totalScores[0].userInfo = userInfo[0];
+
         res.status(200).json({ totalScores });
     } catch (error) {
-        res.status(500).json({ message: "Internal server error" });
+        res.status(500).json({ message: "Internal server error", error: error.message });
     }
-}
+};
 
 export default { getTotalScoresByUserId };
